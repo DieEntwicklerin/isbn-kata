97e2201e613a1dac552361d7796827ae57e547d9
KatharinaSchwarz <katharina.schwarz@it-economics.de>
2023-02-11 11:53:34 +0100

    Mark the method BookRepository.byIsbn(String) as deprecated

--- Java/src/main/java/org/example/domain/BookRepository.java
+++ Java/src/main/java/org/example/domain/BookRepository.java
@@ -4,6 +4,10 @@ import java.util.Optional;
 
 public interface BookRepository {
 
+    /**
+     * @deprecated use {@link #byIsbn(ISBN)} instead. Will be removed with release 1.1
+     */
+    @Deprecated(since = "1.0", forRemoval = true)
     Optional<Book> byIsbn(String isbn);
 
     Optional<Book> byIsbn(ISBN validISBN);
