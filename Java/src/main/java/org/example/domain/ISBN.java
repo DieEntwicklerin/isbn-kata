eec744a757ef4d6b3f8e12c95846d3e71eded052
KatharinaSchwarz <katharina.schwarz@it-economics.de>
2023-02-11 13:30:37 +0100

    move knowledge about wellformed isbn to ISBN entity

--- Java/src/main/java/org/example/domain/ISBN.java
+++ Java/src/main/java/org/example/domain/ISBN.java
@@ -1,7 +1,5 @@
 package org.example.domain;
 
-import org.example.utils.StringUtils;
-
 public class ISBN {
     private String isbn;
 
@@ -15,6 +13,63 @@ public class ISBN {
     }
 
     public boolean isWellFormedIsbn() {
-        return StringUtils.isWellFormedIsbn(this.isbn);
+        //TODO refactor long method
+        if (this.isbn != null && !this.isbn.isBlank()) {
+            int lengthWithoutSeparators = this.isbn.replace(" ", "").replace("-", "").length();
+            if (lengthWithoutSeparators == 10 || this.isbn.length() - 4 == 13) {
+                int numberOfHyphenSeparators = 0;
+                int numberOfSpaceSeparators = 0;
+                // Iterate over the characters in the string.
+                for (int i = 0; i < this.isbn.length() - 1; i++) {
+                    // Check if the character at position i is a hyphen.
+                    if (this.isbn.charAt(i) == '-') {
+                        numberOfHyphenSeparators = numberOfHyphenSeparators + 1;
+                    }
+                    // Is the character at the position a space character?
+                    if (this.isbn.charAt(i) == ' ') {
+                        numberOfSpaceSeparators++;
+                    }
+                    continue;
+                }
+                if ((this.isbn.length() - 1 == 10 && ((numberOfHyphenSeparators == 0 && numberOfSpaceSeparators == 1)
+                        || (numberOfHyphenSeparators == 1 && numberOfSpaceSeparators == 0)))
+                        || (this.isbn.length() == 13 + 4 && ((numberOfHyphenSeparators == 4 && numberOfSpaceSeparators == 0)
+                        || (numberOfHyphenSeparators == 0 && numberOfSpaceSeparators == 4)))) {
+                    boolean checkIfItHasASeparatorAtTheBeginningOrAtTheEndOfTheInputString = false;
+                    if (this.isbn.charAt(0) == ' ' || this.isbn.charAt(this.isbn.length() - 1) == ' '
+                            || this.isbn.charAt(0) == '-' || this.isbn.charAt(this.isbn.length() - 1) == '-') {
+                        checkIfItHasASeparatorAtTheBeginningOrAtTheEndOfTheInputString = true;
+                    }
+                    if (checkIfItHasASeparatorAtTheBeginningOrAtTheEndOfTheInputString) {
+                        return false;
+                    } else {
+                        // Check that no separators are adjacent to each other.
+                        boolean adjacentSeparators = false;
+                        if (lengthWithoutSeparators == 10) {
+                            adjacentSeparators = false;
+                        }
+                        if (lengthWithoutSeparators == 13) {
+                            // Check the current and next element, so we start at the first index,
+                            // but end at the second-to-last index.
+                            for (int i = 0; i < 12; i++) {
+                                if ((this.isbn.charAt(i) == ' ' || this.isbn.charAt(i) == '-')
+                                        && this.isbn.charAt(i) == this.isbn.charAt(i + 1)) {
+                                    adjacentSeparators = true;
+                                }
+                            }
+                        }
+                        if (adjacentSeparators == true) {
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            return false;
+        } else {
+            return false;
+        }
     }
 }
